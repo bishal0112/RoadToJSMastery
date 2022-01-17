29335476aeb9886dcebbdfa1ab52d7fcf671e694
bishal0112 <bishalknight5440@gmail.com>
2022-01-17 21:14:06 +0530

    day1

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// #5101ff#5101ff#5101ff   DAY 1  #5101ff#5101ff#5101ff
+
+console.log("Hello World!");
+
+// Values
+// Values are like booleans, strings, arrays, numbers...
+23 + 24;
+("this is a value");
+
+// Variables
+// They are containers for storing data
+var firstName = "firstName";
+let middleName = "middleName";
+const lastName = "lastName";
+
+console.log(firstName, middleName, lastName);
+
+// Naming Convention
+// Check MD file...
+let x = 10;
+let _name = "maydox";
+let $ = "dollar";
+const PI = 3.14;
+
+// ================================================================
+// ================================================================
---
+++ readme.md
@@ -0,0 +1,24 @@
+## DAY 1
+
+- VS code set-up
+- Intro to JS
+- HELLO WORLD
+- JS Definition
+- Linking JS File(Inline, External)
+- Console.log
+- Developer Console
+- Role of JS
+- JS releases
+- Values and Variables
+- Camel Cases
+- Naming Convention
+- Syntax Error
+- Reserve Keywords
+
+#### Naming Convention
+
+- Name should have only letters, digits, underscores, and dollar signs.
+- Should not start with number.
+- Can have $ or \_ signs
+- JavaScript is case sensitive
+- Reserve words cannot be used as names.
